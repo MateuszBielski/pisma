3e894b57289e761557232d30ae612a8fe3b849e2
Mateusz Bielski <mateo.bass@interia.pl>
2021-08-03 14:19:23 +0200

    rozpoznany tekst można wklejać tez do pola sprawa

--- public/js/odczytOCRfragmentuObrazu.js
+++ public/js/odczytOCRfragmentuObrazu.js
@@ -3,20 +3,31 @@ jQuery(document).ready(function() {
     obrazPng.on("dragstart", function() {
         return false;//wyłącza ikonkę przeciągania obrazu
     });
+    
+
 
     var adresObrazu = obrazPng.attr("src_bez_fg");
-    var polaDoUzupelnienia = $("#pismo_opis");
+    var polaDoUzupelnienia = $("td");
    var x0,x1,y0,y1;
    var img_szerokosc = obrazPng.width();
    var img_wysokosc = obrazPng.height();
    var rozpoznanyTekst = '';
 
+   var canv = $("#myCanvas");
+   var canv_js = document.getElementById("myCanvas");
+   var context = canv_js.getContext('2d');
+   context.globalAlpha = 0.8;
+
+   canv.attr('width',img_szerokosc);
+   canv.attr('height',img_wysokosc);
+//    canv.attr('style',"border:1px solid #000000;");
    
    obrazPng.on('mousedown',function(e){
        x0 = e.pageX - this.offsetLeft;
        y0 = e.pageY - this.offsetTop;
        rozpoznanyTekst = '';
        polaDoUzupelnienia.off('click');
+       context.clearRect(0, 0, img_szerokosc, img_wysokosc);
        //    polaDoUzupelnienia.off('click'); uruchomić
     // console.log("adres"+adresObrazu);
     });
@@ -31,10 +42,16 @@ jQuery(document).ready(function() {
         xp = Math.max(x0,x1)/img_szerokosc;
         yg = Math.min(y0,y1)/img_wysokosc;
         yd = Math.max(y0,y1)/img_wysokosc;
+
         
-        // console.log("start"+xl+", "+yg);
-        // console.log("stop"+xp+", "+yd);
-        // console.log("wymiary: "+img_szerokosc+", "+img_wysokosc);
+
+        context.beginPath();
+        context.rect(x0,y0,x1-x0,y1-y0)
+        context.fillStyle = "blue";
+        context.fill();
+            // context.clearRect(40,40,60,60);
+            // context.strokeRect(45,45,50,50);
+        
         $.ajax({
             url : '/pismo/rozpoznawanieAjax',
             type: 'GET',
@@ -53,8 +70,9 @@ jQuery(document).ready(function() {
                 // console.log(rozpoznanyTekst);
                 polaDoUzupelnienia.on('click',function(){
                     // console.log(jsonResp.odp);
-                    polaDoUzupelnienia.val(function() {
-                        // console.log(this.value)
+                    wybranePoleDoUzupelnienia = $(this).find("textarea");
+                    wybranePoleDoUzupelnienia.val(function() {
+                        
                         //czy spacja bo to kolejny dodawany fragment
                         var istniejacyTekst = this.value;
                         
